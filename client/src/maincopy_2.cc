458ee145f746e814eecd8a2bd01a5c3fe1ebaaff
KabanBaraban <voldemar.sobols.inc@gmail.com>
2024-12-07 20:45:08 +0100

    some examples

--- client/src/maincopy_2.cc
+++ client/src/maincopy_2.cc
@@ -24,6 +24,8 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <string>
+#include <vector>
 
 
 // Custom Blend Modes
@@ -217,18 +219,11 @@ void SetupBoxes(Rectangle *boxes, int *count)
     *count = MAX_BOXES;
 }
 
-#define MAX_BUILDINGS   100
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 
 #define MAX_BUILDINGS   100
-bool key_up = false;
-bool key_down = false;
-bool key_left = false;
-bool key_right = false;
-bool key_atack = false;
+
+Texture2D Background;
+Texture2D Player;
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -237,12 +232,19 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = GetScreenWidth();
-    const int screenHeight = GetScreenHeight();
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    const int sqw = 6;
+    const int sqh = 140;
+    Texture2D sprite;
+    sprite = LoadTexture("client/assets/Sprite-0003");
+    int rotation = 0;
+
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - 2d camera");
 
-    Rectangle player = { 400, 280, 40, 40 };
+    Rectangle player = { screenWidth / 2, screenHeight / 2, sqw, sqh };
     Rectangle buildings[MAX_BUILDINGS] = { 0 };
     Color buildColors[MAX_BUILDINGS] = { 0 };
 
@@ -251,7 +253,7 @@ int main(void)
     for (int i = 0; i < MAX_BUILDINGS; i++)
     {
         buildings[i].width = (float)GetRandomValue(50, 200);
-        buildings[i].height = (float)GetRandomValue(100, 800);
+        buildings[i].height = (float)GetRandomValue(50, 100);
         buildings[i].y = screenHeight - 130.0f - buildings[i].height;
         buildings[i].x = -6000.0f + spacing;
 
@@ -261,7 +263,7 @@ int main(void)
     }
 
     Camera2D camera = { 0 };
-    camera.target = (Vector2){ player.x + 20.0f, player.y + 20.0f };
+    camera.target = (Vector2){ player.x + ((float)sqw / 2), player.y + ((float)sqh / 2)};
     camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
     camera.rotation = 0.0f;
     camera.zoom = 1.0f;
@@ -269,31 +271,52 @@ int main(void)
     SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
     uint8_t keys_tap_saved = 0b00000000;
+    bool key_down = false;
+    bool key_up = false;
+    bool key_left = false;
+    bool key_right = false;
+    bool key_atack = false;
     // Main game loop
+
+
+    Rectangle sourceRec = { 0.0f, 0.0f, (float)sqw, (float)sqh };
+
+    // Destination rectangle (screen rectangle where drawing part of texture)
+    Rectangle destRec = { screenWidth/2.0f, screenHeight/2.0f, sqw*2.0f, sqh*2.0f };
+
+    Vector2 origin = { (float)sqw, (float)sqh };
+
+
+
+    //
+    //
     while (!WindowShouldClose())        // Detect window close button or ESC key
     {
 
-        // Update
-        //----------------------------------------------------------------------------------
-        // Player movement
-         // For send player`s moeves to server
         uint8_t keys_tap_input = keys_tap_saved;
 
         if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
         {
             key_right = true;
+            player.x += 2;
+
         }
         else if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
         {
             key_left = true;
+            player.x -= 2;
+
         }
         else if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W))
         {
             key_up = true;
+            player.y -= 2;
+
         }
         else if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S))
         {
             key_down = true;
+            player.y += 2;
         }
         else if (IsKeyUp(KEY_RIGHT) || IsKeyUp(KEY_D))
         {
@@ -311,6 +334,16 @@ int main(void)
         {
             key_down = false;
         }
+        if(IsKeyDown(KEY_Q))
+        {
+            rotation++;
+        }
+        else if(IsKeyDown(KEY_E))
+        {
+            rotation--;
+        }
+
+
 
         keys_tap_input = key_up * 0b10000 + key_down * 0b1000 + key_left * 0b100 + key_right * 0b10 + key_atack * 0b1;
 
@@ -318,21 +351,15 @@ int main(void)
         if(keys_tap_input != keys_tap_saved)
         {
 
+
             keys_tap_saved = keys_tap_input;
         }
 
 
 
         // Camera target follows player
-        camera.target = (Vector2){ player.x + 20, player.y + 20 };
+        camera.target = (Vector2){ player.x + ((float)sqw / 2) , player.y + ((float)sqh / 2)};
 
-        // Camera rotation controls
-        if (IsKeyDown(KEY_Q)) camera.rotation--;
-        else if (IsKeyDown(KEY_E)) camera.rotation++;
-
-        // Limit camera rotation to 80 degrees (-40 to 40)
-        if (camera.rotation > 40) camera.rotation = 60;
-        else if (camera.rotation < -40) camera.rotation = -60;
 
         // Camera zoom controls
         camera.zoom += ((float)GetMouseWheelMove()*0.05f);
@@ -354,12 +381,21 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
+            DrawTexturePro(sprite, sourceRec, destRec, origin, (float)rotation, BLUE);
+
             BeginMode2D(camera);
 
-                DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY);
+                // DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY);
 
                 for (int i = 0; i < MAX_BUILDINGS; i++) DrawRectangleRec(buildings[i], buildColors[i]);
 
+
+                //Rectangle source= (Rectangle){0,0,50,50};
+                //Rectangle dest = (Rectangle){25,25,50,50};
+                //DrawTexturePro(sprite, sourceRec, destRec, origin, (float)rotation, BLUE);
+                //DrawTexturePro(Background, , (Vector2){player.x, player.y}, 0, WHITE);
+
+                DrawText(TextFormat("Player: %.1f  %.1f", player.x + 20, player.y + 20),  player.x + 20, player.y, 20, DARKGRAY);
                 DrawRectangleRec(player, RED);
 
                 DrawLine((int)camera.target.x, -screenHeight*10, (int)camera.target.x, screenHeight*10, GREEN);
@@ -367,21 +403,10 @@ int main(void)
 
             EndMode2D();
 
-            DrawText("SCREEN AREA", 640, 10, 20, RED);
+            // DrawRectangle( 10, 10, 250, 113, Fade(SKYBLUE, 0.5f));
+            // DrawRectangleLines( 10, 10, 250, 113, BLUE);
 
-            DrawRectangle(0, 0, screenWidth, 5, RED);
-            DrawRectangle(0, 5, 5, screenHeight - 10, RED);
-            DrawRectangle(screenWidth - 5, 5, 5, screenHeight - 10, RED);
-            DrawRectangle(0, screenHeight - 5, screenWidth, 5, RED);
 
-            DrawRectangle( 10, 10, 250, 113, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines( 10, 10, 250, 113, BLUE);
-
-            DrawText("Free 2d camera controls:", 20, 20, 10, BLACK);
-            DrawText("- Right/Left to move Offset", 40, 40, 10, DARKGRAY);
-            DrawText("- Mouse Wheel to Zoom in-out", 40, 60, 10, DARKGRAY);
-            DrawText("- A / S to Rotate", 40, 80, 10, DARKGRAY);
-            DrawText("- R to reset Zoom and Rotation", 40, 100, 10, DARKGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
